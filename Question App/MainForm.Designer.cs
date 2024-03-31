35ca3a951c88cc003593a4274b6c0b1b6a467c82
yvrxslvw <yvrxslvv@gmail.com>
2024-03-31 21:02:34 +0200

    Add project files.

---
+++ Question App/MainForm.Designer.cs
@@ -0,0 +1,29 @@
+﻿namespace Question_App
+{
+	partial class MainForm
+	{
+		private System.ComponentModel.IContainer components = null;
+
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(768, 450);
+			this.Text = "Question App";
+		}
+
+		#endregion
+	}
+}
+
